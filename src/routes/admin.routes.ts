505250c40ef46b49ccf7951519026520d9b600fa
Felipe Silveira <felipelarson@gmail.com>
2022-04-28 10:52:41 +0100

    chore: implemented route delete to admin

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 import { validateMiddleware } from "../middlewares/validateMiddleware";
 import { CreateAdminController } from "../modules/admin/useCases/createAdmin/CreateAdminController";
+import { DeleteAdminController } from "../modules/admin/useCases/deleteAdmin/deleteAdminController";
 import { GetAdminController } from "../modules/admin/useCases/getAdmin/GetAdminController";
 import { LoginAdminController } from "../modules/admin/useCases/loginAdmin/LoginAdminController";
 import { loginSchema, registerAdminSchema } from "../schemas";
@@ -11,6 +12,7 @@ const adminRoutes = Router();
 const createAdminControler = new CreateAdminController();
 const getAdminController = new GetAdminController();
 const loginAdminController = new LoginAdminController();
+const deleteAdminController = new DeleteAdminController();
 
 adminRoutes.get("/", getAdminController.handle);
 adminRoutes.post(
@@ -23,5 +25,6 @@ adminRoutes.post(
   validateMiddleware(loginSchema),
   loginAdminController.handle
 );
+adminRoutes.delete("/:id", deleteAdminController.handle);
 
 export { adminRoutes };
