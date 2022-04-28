887aa3ed1ba03e96d73998904a094fcf19dead61
Felipe Silveira <felipelarson@gmail.com>
2022-04-29 00:36:15 +0100

    refactor: return to created teacher

--- src/modules/teacher/useCases/createTeacher/CreateUserController.ts
+++ src/modules/teacher/useCases/createTeacher/CreateUserController.ts
@@ -5,18 +5,7 @@ import { CreateTeacherUseCase } from "./CreateTeacherUseCase";
 
 class CreateTeacherController {
   async handle(request: Request, response: Response): Promise<Response> {
-    const {
-      biography,
-      cpf,
-      email,
-      github,
-      hour,
-      name,
-      password,
-      phone,
-      photo_url,
-      description,
-    } = request.body;
+    const { biography, cpf, email, github, hour, name, password, phone, photo_url, description } = request.body;
 
     const createTeacherUseCase = container.resolve(CreateTeacherUseCase);
 
@@ -33,7 +22,7 @@ class CreateTeacherController {
       description,
     });
 
-    return response.status(201).send();
+    return response.status(201).json({ message: "Teacher created successfully" });
   }
 }
 
