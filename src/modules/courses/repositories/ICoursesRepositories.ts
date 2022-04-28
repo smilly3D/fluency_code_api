71516cc35419a6e848fae6065d0a8f536264f4d7
Felipe Silveira <felipelarson@gmail.com>
2022-04-28 14:34:50 +0100

    feat: implemented course

--- src/modules/courses/repositories/ICoursesRepositories.ts
+++ src/modules/courses/repositories/ICoursesRepositories.ts
@@ -1,10 +1,6 @@
 import { Courses } from "../entities/Courses";
 
 export interface ICoursesRepositories {
-  findByEmail(email: string): Promise<Courses>;
   findById(id: string): Promise<Courses>;
-  findByCPF(cpf: string): Promise<Courses>;
   list(): Promise<Courses[]>;
-  updatePhoto: (user_id: string, photo: string) => Promise<void>;
-  update: (user_id: string, data: any) => Promise<void>;
 }
