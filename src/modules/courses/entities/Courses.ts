d2b3b84d4a9d2bc0fd7a4dda28f34088dcc2106c
Felipe Silveira <felipelarson@gmail.com>
2022-04-28 15:02:02 +0100

    fix: name database from entities

--- src/modules/courses/entities/Courses.ts
+++ src/modules/courses/entities/Courses.ts
@@ -1,7 +1,7 @@
 import { Entity, PrimaryColumn, Column, CreateDateColumn } from "typeorm";
 import { v4 as uuid } from "uuid";
 
-@Entity("course")
+@Entity("courses")
 export class Courses {
   @PrimaryColumn()
   id: string;
