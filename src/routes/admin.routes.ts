29b38c7658fca7c8be0c1d736ac3e87e2a586859
Felipe Silveira <felipelarson@gmail.com>
2022-04-28 11:17:59 +0100

    feat: implement route admin

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
+import { authenticate } from "../middlewares/authenticateMiddleware";
 import { validateMiddleware } from "../middlewares/validateMiddleware";
 import { CreateAdminController } from "../modules/admin/useCases/createAdmin/CreateAdminController";
 import { GetAdminController } from "../modules/admin/useCases/getAdmin/GetAdminController";
 import { LoginAdminController } from "../modules/admin/useCases/loginAdmin/LoginAdminController";
+import { UpdateadminController } from "../modules/admin/useCases/updateStudents/updateStudentsController";
 import { loginSchema, registerAdminSchema } from "../schemas";
 
 const adminRoutes = Router();
@@ -11,6 +13,7 @@ const adminRoutes = Router();
 const createAdminControler = new CreateAdminController();
 const getAdminController = new GetAdminController();
 const loginAdminController = new LoginAdminController();
+const updateAdminController = new UpdateadminController();
 
 adminRoutes.get("/", getAdminController.handle);
 adminRoutes.post(
@@ -23,5 +26,6 @@ adminRoutes.post(
   validateMiddleware(loginSchema),
   loginAdminController.handle
 );
+adminRoutes.patch("/", authenticate, updateAdminController.handle);
 
 export { adminRoutes };
