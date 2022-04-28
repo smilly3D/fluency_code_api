28ad7169cc99eaf55fc5d71b55c63e6cd04c10a6
Felipe Silveira <felipelarson@gmail.com>
2022-04-29 00:46:52 +0100

    refactor: return unvalid teacher

--- src/modules/teacher/useCases/loginTeacher/LoginTeacherUseCase.ts
+++ src/modules/teacher/useCases/loginTeacher/LoginTeacherUseCase.ts
@@ -12,25 +12,23 @@ class LoginTeacherUseCase {
   constructor(
     @inject("TeacherRepository")
     private teacherRepository: ITeacherRepository
-  ) {}
-  async execute({
-    email,
-    password,
-  }: ILoginTeacherDTO): Promise<string | Error> {
+  ) { }
+  async execute({ email, password }: ILoginTeacherDTO): Promise<string | Error> {
     const teacher = await this.teacherRepository.findByEmail(email);
 
     if (!teacher) {
-      throw new AppError("User already exists", 404);
+      throw new AppError("Wrong email/password", 401);
     }
 
     const hasedPassword = await bcrypt.compare(password, teacher.password);
 
     if (!hasedPassword) {
-      throw new AppError("Invalid Password");
+      throw new AppError("Wrong email/password", 401);
     }
     const { id } = teacher;
-    const accessToken = jwt.sign({ id }, JWT_CONFIG.SECRET_KEY, {
+    const accessToken = jwt.sign({ type: "teacher" }, JWT_CONFIG.SECRET_KEY, {
       expiresIn: JWT_CONFIG.EXPIRES_IN,
+      subject: id,
     });
 
     return accessToken;
