676ea56d5c67c40a8b1f65271fb1ce05ffd80fb1
Felipe Silveira <felipelarson@gmail.com>
2022-04-28 10:43:04 +0100

    feat: implemented use case admin

---
+++ src/modules/admin/useCases/deleteAdmin/deleteAdminUseCase.ts
@@ -0,0 +1,26 @@
+import { inject, injectable } from "tsyringe";
+
+import { AppError } from "../../../../errors/AppError";
+import { IAdminRepository } from "../../repositories/IAdminRepository";
+
+@injectable()
+class DeleteAdminUseCase {
+  constructor(
+    @inject("AdminRepositories")
+    private adminRepository: IAdminRepository
+  ) { }
+
+  async execute(id: string): Promise<string | Error> {
+    const admin = await this.adminRepository.findById(id);
+
+    if (!admin) {
+      throw new AppError("Admin not exists", 404);
+    }
+
+    await this.adminRepository.delete(admin);
+
+    return "Student deleted with success";
+  }
+}
+
+export { DeleteAdminUseCase };
